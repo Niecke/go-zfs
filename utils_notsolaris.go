3053c9ae25c912e58807575b861557a0b5747a3d
Niecke <37218693+Niecke@users.noreply.github.com>
2021-04-24 21:51:08 +0200

    Update utils_notsolaris.go

--- utils_notsolaris.go
+++ utils_notsolaris.go
@@ -12,6 +12,6 @@ var dsPropList = []string{"name", "origin", "used", "available", "mountpoint", "
 var dsPropListOptions = strings.Join(dsPropList, ",")
 
 // List of Zpool properties to retrieve from zpool list command on a non-Solaris platform
-var zpoolPropList = []string{"name", "health", "allocated", "size", "free", "readonly", "dedupratio", "fragmentation", "freeing", "leaked"}
+var zpoolPropList = []string{"name", "health", "allocated", "size", "free", "dedupratio"}
 var zpoolPropListOptions = strings.Join(zpoolPropList, ",")
 var zpoolArgs = []string{"get", zpoolPropListOptions}
